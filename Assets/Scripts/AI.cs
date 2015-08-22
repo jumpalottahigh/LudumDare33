a7d4e7e41353a51bea7986fc94b3c7fda50a87e9
Georgi Yanev <georgiyanev.gy@gmail.com>
2015-08-22 17:25:34 +0300

    Before the first break

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (player.transform.position * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		for (int i = 0; i < 5; i++) {
+			Instantiate(Resources.Load("Prefabs/Sheep"), new Vector3(Random.Range(-25, 25), 1f, Random.Range(-25,25)), Quaternion.identity);
+			Instantiate(Resources.Load("Prefabs/Pigglet"), new Vector3(Random.Range(-25, 25), 1f, Random.Range(-25,25)), Quaternion.identity);
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
