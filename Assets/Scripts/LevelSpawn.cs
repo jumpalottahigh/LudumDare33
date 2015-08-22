1f59dfb98881fa95c7d16e55ddd55d0184f292a4
Georgi Yanev <georgiyanev.gy@gmail.com>
2015-08-22 13:21:11 +0300

    Working on level generation

---
+++ Assets/Scripts/LevelSpawn.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSpawn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		float xPos = 0f;
+		float zPos = 0f;
+		float yPos = 1f;
+
+		bool hasGap = false;
+		float rand = 0f;
+
+		for (int i = 0; i < 30; i++) {
+			Instantiate(Resources.Load("Prefabs/Road"), new Vector3(xPos, yPos, zPos), Quaternion.identity);
+
+			if(Random.value > 0.1f){
+				xPos += 5f;
+				zPos += 5f;
+			} else {
+				rand = Random.Range(10, 15);
+				xPos += rand;
+				zPos += rand;
+			}
+
+			yPos = (Random.Range(15, 31) / 10f);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private float speed;
+	private float rotSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+		speed = 10f;
+		rotSpeed = 105f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*
+		if (Input.GetAxis ("Horizontal") > 0) {
+			this.transform.Translate(Vector3.forward * Time.deltaTime);
+		}*/
+
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate(Vector3.forward * Time.deltaTime * speed);
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate(Vector3.up, Time.deltaTime * -rotSpeed);
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate(Vector3.up, Time.deltaTime * rotSpeed);
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate(Vector3.back * Time.deltaTime * speed);
+		}
+
+		if (Input.GetKey (KeyCode.Space)) {
+			transform.Translate(Vector3.up * Time.deltaTime * speed * 2f);
+		}
+	
+	}
+}
